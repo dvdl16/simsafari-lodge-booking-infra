ef87ef950716fe0dfd05b0ced93d715de8a8b732
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-06-12 21:25:20 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  profile = "jabulani"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "arn:aws:logs:*:*:*",
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zipped_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/api/"
+  output_path = "${path.module}/api/hello-python.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/api/hello-python.zip"
+  function_name = "SimSafari_Lodge_Booking_API"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.9"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
