438cd213091bb6e89626f6449750ec1a0e4f3b86
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-06-12 21:56:04 +0200

    Lambda handler fix

--- api/hello-python.py
+++ api/hello-python.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    message = 'Hello {} !'.format(event['key1'])
+    message = 'Hello {}'.format(event['key1'])
     return {
         'message' : message
     }
--- main.tf
+++ main.tf
@@ -52,10 +52,11 @@ data "archive_file" "zipped_python_code" {
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename      = "${path.module}/api/hello-python.zip"
-  function_name = "SimSafari_Lodge_Booking_API"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.9"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename         = "${path.module}/api/hello-python.zip"
+  source_code_hash = filebase64sha256("${path.module}/api/hello-python.zip")
+  function_name    = "SimSafari_Lodge_Booking_API"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "hello-python.lambda_handler"
+  runtime          = "python3.9"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
