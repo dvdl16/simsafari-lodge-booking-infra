12c55dd1f4767b683e603cda6a5d20e6cbd05044
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-07-24 13:49:58 +0200

    Add DynamoDB support

---
+++ api/src/app/db.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+// Example CLI command to insert new records
+// aws dynamodb put-item --table-name tf-bookings-table --item '{"bookingId": {"S": "e0a71c6a-0f12-4290-9eb1-c00060cf7d65"}, "guestDetails": {"S": "Birthday weekend"}, "houses":  {"SS": ["Laarse", "Rondavel"]}}' --profile=jabulani
+
+// Declare a new DynamoDB instance. Note that this is safe for concurrent
+// use.
+var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("eu-west-1"))
+
+func getItem(bookingId string) (*booking, error) {
+	// Prepare the input for the query.
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("tf-bookings-table"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"BookingId": {
+				S: aws.String(bookingId),
+			},
+		},
+	}
+
+	// Retrieve the item from DynamoDB. If no matching item is found
+	// return nil.
+	result, err := db.GetItem(input)
+	if err != nil {
+		return nil, err
+	}
+	if result.Item == nil {
+		return nil, nil
+	}
+
+	// The result.Item object returned has the underlying type
+	// map[string]*AttributeValue. We can use the UnmarshalMap helper
+	// to parse this straight into the fields of a struct. Note:
+	// UnmarshalListOfMaps also exists if you are working with multiple
+	// items.
+	bk := new(booking)
+	err = dynamodbattribute.UnmarshalMap(result.Item, bk)
+	if err != nil {
+		return nil, err
+	}
+
+	return bk, nil
+}
--- api/src/app/main.go
+++ api/src/app/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type book struct {
-    ISBN   string `json:"isbn"`
-    Title  string `json:"title"`
-    Author string `json:"author"`
+type booking struct {
+	BookingId    string   `json:"bookingId"`
+	UserId       string   `json:"userId"`
+	FromDate     int      `json:"fromDate"`
+	ToDate       int      `json:"toDate"`
+	Houses       []string `json:"houses` // Type SS (String Set) in DynamoDB
+	GuestDetails string   `json:"guestDetails"`
 }
 
-func show() (*book, error) {
-    bk := &book{
-        ISBN:   "978-1420931693",
-        Title:  "The Republic",
-        Author: "Plato",
-    }
+func show() (*booking, error) {
+	// Fetch a specific book record from the DynamoDB database. We'll
+	// make this more dynamic in the next section.
+	bk, err := getItem("e0a71c6a-0f12-4290-9eb1-c00060cf7d65")
+	if err != nil {
+		return nil, err
+	}
 
-    return bk, nil
+	return bk, nil
 }
 
 func main() {
-    lambda.Start(show)
+	lambda.Start(show)
 }
