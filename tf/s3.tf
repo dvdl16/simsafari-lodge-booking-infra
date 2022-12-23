9824387ccd5c0a4f1e91b8e2bce4e28b0756d22e
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-12-23 13:17:31 +0200

    Add resources for S3 website serving

---
+++ tf/s3.tf
@@ -0,0 +1,65 @@
+# S3 bucket for website.
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = "www.${var.bucket_name}"
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_website_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "www_bucket_policy" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+}
+
+# S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = var.bucket_name
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_acl" "root_bucket_acl" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "root_bucket_policy" {
+  bucket = aws_s3_bucket.root_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+}
+
+resource "aws_s3_bucket_website_configuration" "root_bucket" {
+  bucket = aws_s3_bucket.root_bucket.bucket
+
+  redirect_all_requests_to {
+    host_name = "https://www.${var.domain_name}"
+  }
+
+}
