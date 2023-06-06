a2f43edfda053afce569a87df116acf4e6c7221b
Dirk van der Laarse <dirk@laarse.co.za>
2023-06-06 20:27:58 +0200

    fix: passing of aws access key and secret to backends

--- tf/providers.tf
+++ tf/providers.tf
@@ -12,14 +12,10 @@ terraform {
     bucket = "onsjabulani-terraform"
     key    = "prod/terraform.tfstate"
     region = "eu-west-1"
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
   }
 }
 
 provider "aws" {
   alias   = "acm_provider"
   region  = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
