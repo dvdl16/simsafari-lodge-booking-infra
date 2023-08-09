9383a5ea158c8be1e0a0121c2270c5c1a8835d9c
Dirk van der Laarse <dirk@laarse.co.za>
2023-08-09 20:53:17 +0200

    fix: remove keys from provider block

--- tf/providers.tf
+++ tf/providers.tf
@@ -18,6 +18,6 @@ terraform {
 provider "aws" {
   alias   = "acm_provider"
   region  = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
 }
