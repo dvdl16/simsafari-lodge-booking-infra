3baadaaf029266327f86fc83fbfdc067e320e9ad
Dirk van der Laarse <dirk@laarse.co.za>
2025-07-15 19:19:07 +0200

    chore: update deps

--- tf/providers.tf
+++ tf/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.10.3"
+  required_version = "~> 1.12.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.82"
+      version = "~> 5.94"
     }
   }
 
